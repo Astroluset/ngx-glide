9ca181c2d102c9d4748fc257aefbb361a17f6d3e
Alexander Kushnir <kushnirale@gmail.com>
2026-02-05 16:36:08 +0200

    feat: upgrade to angular21

--- apps/ngx-glide-demo/src/main.ts
+++ apps/ngx-glide-demo/src/main.ts
@@ -1,9 +1,9 @@
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserAnimationsModule)],
+  providers: [provideZoneChangeDetection(),importProvidersFrom(BrowserAnimationsModule)],
 }).catch((err) => console.error(err));
